bcf5521a5b964aa0c524a2277e71d6563a43fbdc
rafaScalet <rafaelscalet2018@gmail.com>
2024-07-12 11:29:11 -0300

    closes #4 - implementar as 4 operações

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
-function somar(){
+function calcular(operacao){
     const num1 = Number(document.getElementById('num1').value)
     const num2 = Number(document.getElementById('num2').value)
 
-    const resultado = num1 + num2
+    let resultado
+
+    switch (operacao) {
+        case '+': 
+            resultado = num1 + num2
+            break
+        case '-': 
+            resultado = num1 - num2
+            break
+        case '*':
+            resultado = num1 * num2
+            break
+        case '/':
+            resultado = num1 / num2
+            break
+        default:
+            alert('Operação inválida!')
+            return
+    }
 
     document.getElementById('resultado').value = resultado
 }
